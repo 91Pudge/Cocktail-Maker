5a3d24bb4f8030df1f01fe7d4f3f48eab6a1494f
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-12-15 16:24:55 +0900

    Feat: Create navbar and style

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import "./css_files/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <span className="material-symbols-outlined">local_bar</span>
+      <h1>Cocktail Maker</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
