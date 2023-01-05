7e5bccbe7ef24069963090210a824dfc78c17528
91Pudge <73273137+91Pudge@users.noreply.github.com>
2023-01-05 12:33:23 +0900

    Feat: Create function that request data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  Navigate
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import "./index.css";
+import Footer from "./components/Footer";
 
 function App() {
+  const [cocktailData, setCocktailData] = useState([]);
+  const [userInput, setUserInput] = useState<String>("");
+  const [isLoading, setLoading] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
+  };
+  const onSubmitForm = async (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    try {
+      const choice = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`
+      );
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+      const parseRes = await choice.json();
+      setCocktailData(parseRes.drinks);
+    } catch (err) {
+      console.log("error");
+    }
+  };
+
   return (
     <Router>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/"
+          element={
+            <Home
+              onSubmitForm={onSubmitForm}
+              handleChange={handleChange}
+              cocktailData={cocktailData}
+              userInput={setUserInput}
+              setCocktailData={setCocktailData}
+              isLoading={isLoading}
+              randomCocktail={randomCocktail}
+            />
+          }
+        ></Route>
       </Routes>
+      <Footer />
     </Router>
   );
 }
