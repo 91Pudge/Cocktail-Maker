36a96f2113440e2c87f96ba373423c1eb851c347
91Pudge <73273137+91Pudge@users.noreply.github.com>
2023-01-08 15:11:18 +0900

    Feat: Create drinks type object

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,11 +3,17 @@ import { ChangeEvent } from "react";
 export type Props = {
     onSubmitForm: (e: React.FormEvent<HTMLFormElement>) => void;
     handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    cocktailData: any;
-    setCocktailData: any;
-    userInput: any;
-    isLoading: boolean;
+    cocktailData: [];
+    setCocktailData: React.Dispatch<React.SetStateAction<[]>>;
+    userInput: React.Dispatch<React.SetStateAction<string>>;
     randomCocktail: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    data: any;
     id: number;
+
 };
+
+export type Drinks = {
+    strDrink: string;
+    cocktailData: any;
+    id: number;
+    data: any;
+} & Props;
