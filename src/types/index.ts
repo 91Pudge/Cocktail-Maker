1bab37ce2ed0ff29cbd61140f3673028db6c8423
91Pudge <73273137+91Pudge@users.noreply.github.com>
2023-01-09 18:01:18 +0900

    Chore: Add variables types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,19 +1,50 @@
 import { ChangeEvent } from "react";
 
 export type Props = {
-    onSubmitForm: (e: React.FormEvent<HTMLFormElement>) => void;
-    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    cocktailData: [];
-    setCocktailData: React.Dispatch<React.SetStateAction<[]>>;
-    userInput: React.Dispatch<React.SetStateAction<string>>;
-    randomCocktail: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    onSubmitForm?: (e: React.FormEvent<HTMLFormElement>) => void;
+    handleChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+    cocktailData: any[];
+    setCocktailData?: React.Dispatch<React.SetStateAction<any[]>>;
+    userInput?: React.Dispatch<React.SetStateAction<string>>;
+    randomCocktail?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
     id: number;
+    data?: any[];
 
 };
 
 export type Drinks = {
     strDrink: string;
+    id: number;
+    data: any;
+    idDrink: string;
+    strInstructions: string;
+    strMeasure1: string;
+    strMeasure2: string;
+    strMeasure3: string;
+    strMeasure4: string;
+    strMeasure5: string;
+    strMeasure6: string;
+    strDrinkThumb: string;
+    strGlass: string;
+};
+
+
+
+export type IRecipe = {
     cocktailData: any;
     id: number;
     data: any;
-} & Props;
+}
+
+export type HomeProps = {
+    onSubmitForm: (e: React.FormEvent<HTMLFormElement>) => void;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    cocktailData: any[];
+    setCocktailData: React.Dispatch<React.SetStateAction<any[]>>;
+    userInput: React.Dispatch<React.SetStateAction<string>>;
+    randomCocktail: (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => void;
+    id: number;
+    data: any[];
+};
